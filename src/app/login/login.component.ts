7d2b2d501981ee5b79857029586a1628b8a9904f
John VG <John.Vg@soti.net>
2023-09-21 15:16:05 +0530

    updated authentication api in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiServicesService } from '../Shared/api-services.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { LoginModel } from '../models/login-model';
+import { Router } from '@angular/router';
+import { AuthenticatedResponse } from '../models/authenticated-response';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  //for login
+  invalidLogin?: boolean;
+  credentials: LoginModel = {username:'', password:''};
+
+  constructor(
+    private router: Router, 
+    private http: HttpClient,
+    private formBuilder:FormBuilder,
+    serv :ApiServicesService
+    ) { }
+
+  //
 
   loginForm!:FormGroup;
-  constructor (private formBuilder:FormBuilder,serv :ApiServicesService){
+  
 
-  }
+  
   
 
   ngOnInit(): void {
@@ -31,6 +48,24 @@ export class LoginComponent {
       this.loginForm.reset();
       alert("Incorrect username or password")
      } 
+    }
+
+    //for loginin;
+    login = ( form: NgForm) => {
+      if (form.valid) {
+        this.http.post<AuthenticatedResponse>("http://localhost:5179/api/auth/login", this.credentials, {
+          headers: new HttpHeaders({ "Content-Type": "application/json"})
+        })
+        .subscribe({
+          next: (response: AuthenticatedResponse) => {
+            const token = response.token;
+            localStorage.setItem("jwt", token); 
+            this.invalidLogin = false; 
+            this.router.navigate(["/"]);
+          },
+          error: (err: HttpErrorResponse) => this.invalidLogin = true
+        })
+      }
     }
 
 
--- src/app/models/authenticaed-responcse.ts
+++
@@ -1,3 +0,0 @@
-export interface AuthenticaedResponcse {
-    token: string;
-}
