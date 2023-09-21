3c13250d39d92aabd299619a6d1e98087fd7df41
John VG <John.Vg@soti.net>
2023-09-21 15:58:32 +0530

    made changes to login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component,OnInit } from '@angular/core';
-=======
 import { Component, OnInit } from '@angular/core';
->>>>>>> 7d2b2d501981ee5b79857029586a1628b8a9904f
 import { ApiServicesService } from '../Shared/api-services.service';
 import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
@@ -16,12 +12,11 @@ import { AuthenticatedResponse } from '../models/authenticated-response';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
-<<<<<<< HEAD
+
 
   loginForm!:FormGroup;
   submittedData:any[]=[];
-  constructor (private formBuilder:FormBuilder,serv :ApiServicesService){
-=======
+  
 
   //for login
   invalidLogin?: boolean;
@@ -36,40 +31,33 @@ export class LoginComponent implements OnInit{
 
   //
 
-  loginForm!:FormGroup;
-  
->>>>>>> 7d2b2d501981ee5b79857029586a1628b8a9904f
-
-  
-  
-
   ngOnInit(): void {
     this.loginForm=this.formBuilder.group({
-      username:['',Validators.required,Validators.email],
+      username:['',[Validators.required,Validators.email]],
       password:['',Validators.required],
       
       
     });
     }
-    onSubmit(){
-     if(this.loginForm.valid){
-      const formData=this.loginForm.value;
-      this.submittedData.push(formData);
-      this.loginForm.reset();
-      // console.log(this.loginForm.get('username')?.value);
-      // this.loginForm.reset();
-      alert("Login Complete")
-     }
-     else{
-      this.loginForm.reset();
-      alert("Incorrect username or password")
-     } 
-    }
+    // onSubmit(){
+    //  if(this.loginForm.valid){
+    //   const formData=this.loginForm.value;
+    //   this.submittedData.push(formData);
+    //   this.loginForm.reset();
+    //   // console.log(this.loginForm.get('username')?.value);
+    //   // this.loginForm.reset();
+    //   alert("Login Complete")
+    //  }
+    //  else{
+    //   this.loginForm.reset();
+    //   alert("Incorrect username or password")
+    //  } 
+    // }
 
     //for loginin;
-    login = ( form: NgForm) => {
+    login = ( form: FormGroup) => {
       if (form.valid) {
-        this.http.post<AuthenticatedResponse>("http://localhost:5179/api/auth/login", this.credentials, {
+        this.http.post<AuthenticatedResponse>("http://localhost:5092/api/auth/login", this.credentials, {
           headers: new HttpHeaders({ "Content-Type": "application/json"})
         })
         .subscribe({
@@ -79,7 +67,10 @@ export class LoginComponent implements OnInit{
             this.invalidLogin = false; 
             this.router.navigate(["/"]);
           },
-          error: (err: HttpErrorResponse) => this.invalidLogin = true
+          error: (err: HttpErrorResponse) => {
+            this.invalidLogin = true,
+            alert("Invalid username or Password")
+          }
         })
       }
     }
