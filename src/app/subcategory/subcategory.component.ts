93a58ae6c34663e6ae387f3de909b554deac894d
Annmerin Varghese <2001annmerin@gmail.com>
2023-09-23 20:55:19 +0530

    Product

---
+++ src/app/subcategory/subcategory.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { ApiServicesService } from '../Shared/api-services.service';
+import { Product } from '../models/product';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-subcategory',
+  templateUrl: './subcategory.component.html',
+  styleUrls: ['./subcategory.component.css']
+})
+export class SubcategoryComponent implements OnInit{
+  constructor(private router: Router,private route: ActivatedRoute, public serv: ApiServicesService) {}
+
+  
+ngOnInit(): void {
+  
+ // Get the category ID from the route parameters
+ this.route.paramMap.subscribe(params => {
+  const categoryId = +params.get('Id'); // "+" is used to convert the parameter to a number
+  
+  // Now, you can use the categoryId to fetch products based on it
+  this.serv.GetProductByCategory(categoryId);
+});
+  
+}  
+onProductClick(productId: number){
+  
+  // Navigate to the ProductdetailComponent with the productId parameter
+  this.router.navigate(['productdetail']);
+}
+}
